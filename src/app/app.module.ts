c7b6def5281728408feb947086f6876adbef2abc
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-06-04 10:11:33 -0400

    passing arguments to routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { RouterModule, Routes } from '@angular/router';
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'users', component: UsersComponent},
+  {path: 'users/:userId/:userName', component: UserComponent},
   {path: 'servers', component: ServersComponent}
 ];
 
