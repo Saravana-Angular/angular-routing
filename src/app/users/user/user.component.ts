b79bd388d5fa09d753084c2cdad05af5f4d53ad5
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-06-04 13:44:41 -0400

    implemented explicty ngOnDestroy method to unsubscribe from the params

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
-
+  paramsSubscription: Subscription;
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -17,11 +18,15 @@ export class UserComponent implements OnInit {
       name: this.route.snapshot.params['userName']
     }
 
-    this.route.params.subscribe((params: Params)=> {
+    this.paramsSubscription = this.route.params.subscribe((params: Params)=> {
       this.user.id = params['userId'];
       this.user.name = params['userName'];
     })
 
+  }
+
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
   }
 
 }
